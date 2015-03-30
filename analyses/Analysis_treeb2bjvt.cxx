fd5bd23ea70e5928528a7dd290002f7243889363
Francesco Rubbo <rubbo@atlint03.slac.stanford.edu>
2015-03-30 08:27:28 -0700

    add different matching OPs

--- analyses/Analysis_treeb2bjvt.cxx
+++ analyses/Analysis_treeb2bjvt.cxx
@@ -51,6 +51,10 @@
   jrpt     = new vector<float>();
   jb2bpt  = new vector<float>();
   jb2bjvt  = new vector<float>();
+  jb2bjvt_70  = new vector<float>();
+  jb2bjvt_75  = new vector<float>();
+  jb2bjvt_85  = new vector<float>();
+  jb2bjvt_90  = new vector<float>();
   jb2bcorrjvf  = new vector<float>();
   jb2brpt  = new vector<float>();
   jb2bpt_trk2  = new vector<float>();
@@ -64,6 +68,26 @@
   tjpt     = new vector<float>();  
   jishs     = new vector<bool>();  
   jispu     = new vector<bool>();  
+
+  trk2jpt     = new vector<float>();  
+  trk2jeta     = new vector<float>();  
+  trk2jphi     = new vector<float>();
+  trk2jjvt     = new vector<float>();
+  trk2jcorrjvf = new vector<float>();
+  trk2jrpt     = new vector<float>();
+  ttrk2jpt     = new vector<float>();  
+  trk2jishs     = new vector<bool>();  
+  trk2jispu     = new vector<bool>();  
+
+  trk4jpt     = new vector<float>();  
+  trk4jeta     = new vector<float>();  
+  trk4jphi     = new vector<float>();
+  trk4jjvt     = new vector<float>();
+  trk4jcorrjvf = new vector<float>();
+  trk4jrpt     = new vector<float>();
+  ttrk4jpt     = new vector<float>();  
+  trk4jishs     = new vector<bool>();  
+  trk4jispu     = new vector<bool>();  
 
   truejetpt     = new vector<float>();  
   truejeteta     = new vector<float>();  
@@ -108,6 +132,10 @@ void Analysis_treeb2bjvt::WorkerTerminate()
   delete jcorrjvf    ;  
   delete jb2bpt ;  
   delete jb2bjvt ;  
+  delete jb2bjvt_70 ;  
+  delete jb2bjvt_75 ;  
+  delete jb2bjvt_85 ;  
+  delete jb2bjvt_90 ;  
   delete jb2bcorrjvf ;  
   delete jb2brpt ;  
   delete jb2bpt_trk2 ;  
@@ -122,6 +150,26 @@ void Analysis_treeb2bjvt::WorkerTerminate()
   delete jishs    ;  
   delete jispu   ;  
 
+  delete trk2jpt    ;  
+  delete trk2jeta    ;  
+  delete trk2jphi    ;  
+  delete trk2jjvt    ;  
+  delete trk2jrpt    ;  
+  delete trk2jcorrjvf    ;  
+  delete ttrk2jpt    ;  
+  delete trk2jishs    ;  
+  delete trk2jispu   ;  
+
+  delete trk4jpt    ;  
+  delete trk4jeta    ;  
+  delete trk4jphi    ;  
+  delete trk4jjvt    ;  
+  delete trk4jrpt    ;  
+  delete trk4jcorrjvf    ;  
+  delete ttrk4jpt    ;  
+  delete trk4jishs    ;  
+  delete trk4jispu   ;  
+
   delete truejetpt    ;  
   delete truejeteta    ;  
   delete truejetphi    ;  
@@ -135,6 +183,10 @@ void Analysis_treeb2bjvt::FillTree(const MomKey Key){
   FillEventVars(fTree);
   for(int i=0; i<jets("AntiKt4LCTopo"); ++i)
     FillJetVars(fTree, i, "AntiKt4LCTopo");
+  for(int i=0; i<jets("AntiKt2tracks"); ++i)
+    FillTrk2JetVars(fTree, i, "AntiKt2tracks");
+  for(int i=0; i<jets("AntiKt4tracks"); ++i)
+    FillTrk4JetVars(fTree, i, "AntiKt4tracks");
   for(int i=0; i<jets("AntiKt4Truth"); ++i)
     FillTrueJetVars(fTree, i, "AntiKt4Truth");
   
@@ -166,6 +218,10 @@ void Analysis_treeb2bjvt::FillTree(const MomKey Key){
    tree->Branch("jrpt","std::vector<float>",&jrpt);
    tree->Branch("jb2bpt","std::vector<float>",&jb2bpt);
    tree->Branch("jb2bjvt","std::vector<float>",&jb2bjvt);
+   tree->Branch("jb2bjvt_70","std::vector<float>",&jb2bjvt_70);
+   tree->Branch("jb2bjvt_75","std::vector<float>",&jb2bjvt_75);
+   tree->Branch("jb2bjvt_85","std::vector<float>",&jb2bjvt_85);
+   tree->Branch("jb2bjvt_90","std::vector<float>",&jb2bjvt_90);
    tree->Branch("jb2bcorrjvf","std::vector<float>",&jb2bcorrjvf);
    tree->Branch("jb2brpt","std::vector<float>",&jb2brpt);
    tree->Branch("jb2bpt_trk2","std::vector<float>",&jb2bpt_trk2);
@@ -179,6 +235,26 @@ void Analysis_treeb2bjvt::FillTree(const MomKey Key){
    tree->Branch("tjpt","std::vector<float>",&tjpt);
    tree->Branch("jispu","std::vector<bool>",&jispu);
    tree->Branch("jishs","std::vector<bool>",&jishs);
+
+   tree->Branch("trk2jpt","std::vector<float>", &trk2jpt);
+   tree->Branch("trk2jeta","std::vector<float>",&trk2jeta);
+   tree->Branch("trk2jphi","std::vector<float>",&trk2jphi);
+   tree->Branch("trk2jjvt","std::vector<float>",&trk2jjvt);
+   tree->Branch("trk2jcorrjvf","std::vector<float>",&trk2jcorrjvf);
+   tree->Branch("trk2jrpt","std::vector<float>",&trk2jrpt);
+   tree->Branch("ttrk2jpt","std::vector<float>",&ttrk2jpt);
+   tree->Branch("trk2jispu","std::vector<bool>",&trk2jispu);
+   tree->Branch("trk2jishs","std::vector<bool>",&trk2jishs);
+
+   tree->Branch("trk4jpt","std::vector<float>", &trk4jpt);
+   tree->Branch("trk4jeta","std::vector<float>",&trk4jeta);
+   tree->Branch("trk4jphi","std::vector<float>",&trk4jphi);
+   tree->Branch("trk4jjvt","std::vector<float>",&trk4jjvt);
+   tree->Branch("trk4jcorrjvf","std::vector<float>",&trk4jcorrjvf);
+   tree->Branch("trk4jrpt","std::vector<float>",&trk4jrpt);
+   tree->Branch("ttrk4jpt","std::vector<float>",&ttrk4jpt);
+   tree->Branch("trk4jispu","std::vector<bool>",&trk4jispu);
+   tree->Branch("trk4jishs","std::vector<bool>",&trk4jishs);
 
    tree->Branch("truejetpt","std::vector<float>",&truejetpt);
    tree->Branch("truejeteta","std::vector<float>",&truejeteta);
@@ -210,6 +286,10 @@ void Analysis_treeb2bjvt::ResetBranches(TTree *tree){
   jrpt     ->clear();  
   jb2bpt  ->clear();  
   jb2bjvt  ->clear();  
+  jb2bjvt_70  ->clear();  
+  jb2bjvt_75  ->clear();  
+  jb2bjvt_85  ->clear();  
+  jb2bjvt_90  ->clear();  
   jb2bcorrjvf  ->clear();  
   jb2brpt  ->clear();  
   jb2bpt_trk2  ->clear();  
@@ -223,6 +303,26 @@ void Analysis_treeb2bjvt::ResetBranches(TTree *tree){
   tjpt     ->clear();  
   jispu    ->clear();  
   jishs    ->clear();  
+
+  trk2jpt     ->clear();  
+  trk2jeta     ->clear();  
+  trk2jphi     ->clear();  
+  trk2jjvt     ->clear();  
+  trk2jcorrjvf     ->clear();  
+  trk2jrpt     ->clear();  
+  ttrk2jpt     ->clear();  
+  trk2jispu    ->clear();  
+  trk2jishs    ->clear();  
+
+  trk4jpt     ->clear();  
+  trk4jeta     ->clear();  
+  trk4jphi     ->clear();  
+  trk4jjvt     ->clear();  
+  trk4jcorrjvf     ->clear();  
+  trk4jrpt     ->clear();  
+  ttrk4jpt     ->clear();  
+  trk4jispu    ->clear();  
+  trk4jishs    ->clear();  
 
   truejetpt     ->clear();  
   truejeteta     ->clear();  
@@ -279,6 +379,10 @@ void Analysis_treeb2bjvt::FillJetVars(TTree* tree, int jindex,
   if(myjet->Objs("jetsBtoB")==1) jb2bpt->push_back(b2bjet->p.Pt());
   else jb2bpt->push_back(-99);
   jb2bjvt->push_back(myjet->Float("BtoBJVT"));
+  jb2bjvt_70->push_back(myjet->Float("BtoBJVT_70"));
+  jb2bjvt_75->push_back(myjet->Float("BtoBJVT_75"));
+  jb2bjvt_85->push_back(myjet->Float("BtoBJVT_85"));
+  jb2bjvt_90->push_back(myjet->Float("BtoBJVT_90"));
   jb2bcorrjvf->push_back(myjet->Float("BtoBcorrJVF"));
   jb2brpt->push_back(myjet->Float("BtoBRpT"));
   if(myjet->Objs("jetsBtoB_trk2")==1) jb2bpt_trk2->push_back(b2bjet_trk2->p.Pt());
@@ -290,9 +394,59 @@ void Analysis_treeb2bjvt::FillJetVars(TTree* tree, int jindex,
   else jb2bpt_trk4->push_back(-99);
   jb2bjvt_trk4->push_back(myjet->Float("BtoBJVT_trk4"));
   jb2bcorrjvf_trk4->push_back(myjet->Float("BtoBcorrJVF_trk4"));
-  jb2brpt_trk4->push_back(myjet->Float("BtoBRpT_trk4"))
-;
+  jb2brpt_trk4->push_back(myjet->Float("BtoBRpT_trk4"));
+
   if(Debug()) cout <<"Analysis_treeb2bjvt::FillTrkVars End" << endl;
+  return;
+}
+
+void Analysis_treeb2bjvt::FillTrk2JetVars(TTree* tree, int jindex,
+				const MomKey jetkey){
+  if(Debug()) cout <<"Analysis_treeb2bjvt::FillTrk2JetVars Begin" << endl;
+
+  Particle  *myjet         = &(jet(jindex,jetkey));
+
+  trk2jpt ->push_back(myjet->p.Pt());
+  if(myjet->Bool("isHSJet")){
+    Particle *ttrk2jet = (Particle*) myjet->Obj("AntiKt2Truth_match");
+    ttrk2jpt ->push_back(ttrk2jet->p.Pt());}
+  else
+    ttrk2jpt ->push_back(-1);
+  trk2jispu->push_back(myjet->Bool("isPUJet"));
+  trk2jishs->push_back(myjet->Bool("isHSJet"));
+  
+  trk2jeta->push_back(myjet->p.Eta());
+  trk2jphi->push_back(myjet->p.Phi());
+  trk2jjvt->push_back(myjet->Float("JVT"));
+  trk2jcorrjvf->push_back(myjet->Float("corrJVF"));
+  trk2jrpt->push_back(myjet->Float("RpT"));
+
+  if(Debug()) cout <<"Analysis_treeb2bjvt::FillTrk2JetVars End" << endl;
+  return;
+}
+
+void Analysis_treeb2bjvt::FillTrk4JetVars(TTree* tree, int jindex,
+				const MomKey jetkey){
+  if(Debug()) cout <<"Analysis_treeb2bjvt::FillTrk4JetVars Begin" << endl;
+
+  Particle  *myjet         = &(jet(jindex,jetkey));
+
+  trk4jpt ->push_back(myjet->p.Pt());
+  if(myjet->Bool("isHSJet")){
+    Particle *ttrk4jet = (Particle*) myjet->Obj("AntiKt4Truth_match");
+    ttrk4jpt ->push_back(ttrk4jet->p.Pt());}
+  else
+    ttrk4jpt ->push_back(-1);
+  trk4jispu->push_back(myjet->Bool("isPUJet"));
+  trk4jishs->push_back(myjet->Bool("isHSJet"));
+  
+  trk4jeta->push_back(myjet->p.Eta());
+  trk4jphi->push_back(myjet->p.Phi());
+  trk4jjvt->push_back(myjet->Float("JVT"));
+  trk4jcorrjvf->push_back(myjet->Float("corrJVF"));
+  trk4jrpt->push_back(myjet->Float("RpT"));
+
+  if(Debug()) cout <<"Analysis_treeb2bjvt::FillTrk4JetVars End" << endl;
   return;
 }
 
--- analyses/Analysis_treeb2bjvt.h
+++ analyses/Analysis_treeb2bjvt.h
@@ -47,6 +47,8 @@ class Analysis_treeb2bjvt : public Analysis_JetMET_Base {
   void FillEventVars(TTree *tree);
   void FillClVars(TTree *tree, int jindex, const MomKey JetKey);
   void FillJetVars(TTree *tree, int jindex, const MomKey JetKey);
+  void FillTrk2JetVars(TTree *tree, int jindex, const MomKey JetKey);
+  void FillTrk4JetVars(TTree *tree, int jindex, const MomKey JetKey);
   void FillTrueJetVars(TTree *tree, int jindex, const MomKey JetKey);
   void FillTrkVars(TTree *tree, int jindex);
   void ResetBranches(TTree *tree);
@@ -69,6 +71,10 @@ class Analysis_treeb2bjvt : public Analysis_JetMET_Base {
   vector<float>   *jrpt;
   vector<float>   *jb2bpt;
   vector<float>   *jb2bjvt;
+  vector<float>   *jb2bjvt_70;
+  vector<float>   *jb2bjvt_75;
+  vector<float>   *jb2bjvt_85;
+  vector<float>   *jb2bjvt_90;
   vector<float>   *jb2bcorrjvf;
   vector<float>   *jb2brpt;
   vector<float>   *jb2bpt_trk2;
@@ -82,6 +88,26 @@ class Analysis_treeb2bjvt : public Analysis_JetMET_Base {
   vector<float>   *tjpt;
   vector<bool>   *jispu;
   vector<bool>   *jishs;
+
+  vector<float>   *trk2jpt;
+  vector<float>   *trk2jeta;
+  vector<float>   *trk2jphi;
+  vector<float>   *trk2jjvt;
+  vector<float>   *trk2jcorrjvf;
+  vector<float>   *trk2jrpt;
+  vector<float>   *ttrk2jpt;
+  vector<bool>   *trk2jispu;
+  vector<bool>   *trk2jishs;
+
+  vector<float>   *trk4jpt;
+  vector<float>   *trk4jeta;
+  vector<float>   *trk4jphi;
+  vector<float>   *trk4jjvt;
+  vector<float>   *trk4jcorrjvf;
+  vector<float>   *trk4jrpt;
+  vector<float>   *ttrk4jpt;
+  vector<bool>   *trk4jispu;
+  vector<bool>   *trk4jishs;
 
   vector<float>   *truejetpt;
   vector<float>   *truejeteta;
